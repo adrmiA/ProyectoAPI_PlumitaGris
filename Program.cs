30eb5fec77c78e9921f275bda9febc57dd6b2e2a
Adriana Bonilla <adrmi.arc@gmail.com>
2026-09-02 17:11:39 -0600

    Add JSON options to ignore reference cycles

--- Program.cs
+++ Program.cs
@@ -3,7 +3,11 @@ using PlumitaGrisAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 builder.Services.AddDbContext<PlumitaGrisContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
